464257e7e2bb70acad905b0bf5f75eb0d2a3de46
Clae1 <claeone1@gmail.com>
2024-10-20 05:41:00 +1300

    Added Inventory to the scene, just need to fix the layout and the object

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public InventoryObject inventory;
-    // Start is called before the first frame update
+    public Collider collider1;
+
+    // Start is called before the first frame update   
+
+    void Start()
+    {
+        // Check if the player has a BoxCollider component attached
+        if (!GetComponent<BoxCollider>())
+        {
+            Debug.LogError("Player object is missing a BoxCollider component. This script requires a BoxCollider for collision detection.");
+        }
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         var item = other.GetComponent<GroundItem>();
         if (item)
         {
             inventory.AddItem(new Item(item.item), 1);
-            Destroy(other.gameObject);
+            Destroy(other.gameObject);   
+
+
+            // Debug message to indicate collision with GroundItem
+            Debug.Log("Player collided with GroundItem: " + item.name);
         }
     }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -26,8 +43,10 @@ public class Player : MonoBehaviour
             inventory.Load();
         }
     }
+
     private void OnApplicationQuit()
     {
-        inventory.Container.Items.Clear();
+        inventory.Container.Items.Clear();   
+
     }
 }
